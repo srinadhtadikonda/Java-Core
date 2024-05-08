ac30e0c3334c9a201848cb2b3c8f196f20fe9a0f
SRINADH TADIKONDA <44929625+srinadhtadikonda@users.noreply.github.com>
2024-05-08 18:53:29 +0530

    Update Test50.java

--- Test50.java
+++ Test50.java
@@ -10,9 +10,9 @@ System.out.println("string1="+str1);
 System.out.println("String2="+str2);
 System.out.println("Length of S1="+str1.length());
 System.out.println("Length of S1="+str1.toUpperCase());
-System.out.println("Length of S2="+str1.toLowerCase());
+System.out.println("Length of S2="+str2.toLowerCase());
 System.out.println("Length of S1="+str1.charAt(2));
-System.out.println("Resplace="+str3.replace('l','x'));
+System.out.println("Replace="+str3.replace('l','x'));
 System.out.println("index="+str1.indexOf('c'));
 System.out.println("substring="+str1.substring(0,3));
 }
