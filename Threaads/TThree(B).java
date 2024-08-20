5d47270cd1283ba2d9f368978c939792cbfd0106
SRINADH TADIKONDA <44929625+srinadhtadikonda@users.noreply.github.com>
2024-08-20 09:26:32 +0530

    Create TThree(B).java

---
+++ Threaads/TThree(B).java
@@ -0,0 +1,17 @@
+import java.lang.*;
+class TThree extends Thread
+{
+public void run()
+{
+System.out.println("Thread is running"+Thread.currentThread());
+}
+class Main 
+{
+public static void main(String args[])
+{
+TThree T1=new TThree();
+TThree T2=new TThree();
+T1.start();
+T2.start();
+}
+}
