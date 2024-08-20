2153dbea21105ac14c7a6a74935fb776355b6e89
SRINADH TADIKONDA <44929625+srinadhtadikonda@users.noreply.github.com>
2024-08-20 09:29:47 +0530

    Update TThree(B).java

--- Threaads/TThree(B).java
+++ Threaads/TThree(B).java
@@ -1,17 +1,21 @@
 import java.lang.*;
-class TThree extends Thread
+class TThree implements Runnable
 {
 public void run()
 {
 System.out.println("Thread is running"+Thread.currentThread());
+}
 }
 class Main 
 {
 public static void main(String args[])
 {
 TThree T1=new TThree();
+Thread TT1=new Thread(T1);
 TThree T2=new TThree();
-T1.start();
-T2.start();
+Thread TT2=new Thread(T2);
+TT1.start();
+TT2.start();
 }
 }
+
