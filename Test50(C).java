8333d40cc08cbe4e10c26271d86534266cd5101f
SRINADH TADIKONDA <44929625+srinadhtadikonda@users.noreply.github.com>
2024-12-26 10:55:05 +0530

    Create Test50(C).java

---
+++ Test50(C).java
@@ -0,0 +1,17 @@
+package corejavaprograms;
+import java.lang.*;
+import java.util.*;
+import java.util.*;
+class Test
+{
+	public static void main(String[] args) 
+	{
+		String str1="liril";
+		String str2="LIRIL";
+		String str3="liril";
+		System.out.println(str1.compareTo(str2));
+		System.out.println(str1.compareTo(str3));
+		System.out.println(str1.compareToIgnoreCase(str2));
+		}
+}
+
