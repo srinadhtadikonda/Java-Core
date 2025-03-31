14ffd094789d58eda3201aa3bd03997c3bb36158
SRINADH TADIKONDA <44929625+srinadhtadikonda@users.noreply.github.com>
2025-03-31 08:29:39 +0530

    Update TSeven.java

--- Threaads/TSeven.java
+++ Threaads/TSeven.java
@@ -1,28 +1,22 @@
 import java.lang.*;
-class TestThreadOne implements Runnable
-{
-public void run()
-{
-try
-{
-int i;
-for(i=1;i<=10;i++)
-{
-Thread.sleep(1500);
-System.out.println(i);
+
+class TestThreadOne implements Runnable {
+    public void run() {
+        try {
+            for (int i = 1; i <= 10; i++) {
+                Thread.sleep(1500);
+                System.out.println(i);
+            }
+        } catch (InterruptedException ex) {
+            System.out.println(ex);
+        }
+    }
 }
-}catch(InterruptedException ex)
-{
-System.out.println(ex);
-}
-}
-}
-class Main
-{
-public static void main(String args[])
-{
-TestThreadOne TTO=new TestThreadOne();
-Thread T=new Thread(TTO);
-TTO.run();
-}
+
+class Main {
+    public static void main(String args[]) {
+        TestThreadOne TTO = new TestThreadOne();
+        Thread T = new Thread(TTO);
+        T.start(); // Start the thread instead of calling run() directly
+    }
 }
