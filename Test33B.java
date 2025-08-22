3fb291103b556f95c399027c9355bbb4a367ea15
SRINADH TADIKONDA <44929625+srinadhtadikonda@users.noreply.github.com>
2025-08-22 17:16:12 +0530

    Create Test33B.java

---
+++ Test33B.java
@@ -0,0 +1,21 @@
+/* program to print 1 to n numbers */
+package corejavaprograms;
+
+public class Test31
+{
+	public static void main(String args[])
+{
+int ch1=65;
+while(ch1<=69)
+{
+	int ch2=65;
+	while(ch2<=69)
+	{	
+System.out.print("\t"+(char)(ch1));
+ch2=ch2+1;
+}
+	ch1=ch1+1;
+	System.out.println();
+}
+}
+}
