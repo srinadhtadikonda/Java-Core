31adbb83251ae17146a8ce5908174e3f8dffc6e7
SRINADH TADIKONDA <44929625+srinadhtadikonda@users.noreply.github.com>
2025-08-22 17:38:51 +0530

    Create Test33D.java

---
+++ Test33D.java
@@ -0,0 +1,18 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class Main {
+    public static void main(String[] args)
+    {
+        int i,j;
+        for(i=0;i<5;i++)
+        {
+            for(j=0;j<5;j++)
+            System.out.print("*");
+   
+            System.out.println();
+        }
+
+}
+    }
+
