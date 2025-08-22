21561aea9dc72a67318f3b0cc4779b783062f422
SRINADH TADIKONDA <44929625+srinadhtadikonda@users.noreply.github.com>
2025-08-22 17:09:50 +0530

    Create Test29A.java

---
+++ Test29A.java
@@ -0,0 +1,15 @@
+/* program to print 1 to n numbers */
+package corejavaprograms;
+
+public class Test31
+{
+	public static void main(String args[])
+{
+int ch=65;
+while(ch<=90)
+{
+System.out.print("\t"+(char)(ch));
+ch=ch+1;
+}
+}
+}
