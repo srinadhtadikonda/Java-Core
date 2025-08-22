a5714eebe00b7141cc00122d94514cb708a37f94
SRINADH TADIKONDA <44929625+srinadhtadikonda@users.noreply.github.com>
2025-08-22 17:26:16 +0530

    Create Test33C.java

---
+++ Test33C.java
@@ -0,0 +1,21 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class Main {
+    public static void main(String[] args)
+    {
+int i=65;
+while(i<=69)
+{
+    int j=65;
+    while(j<=69)
+    {
+        System.out.print("\t"+(char)(j));
+        j=j+1;
+    }
+    i=i+1;
+    System.out.println();
+
+}
+    }
+}
