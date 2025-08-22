d1c5f1528a2f036f2f9b635ff56b7184339fb8a3
SRINADH TADIKONDA <44929625+srinadhtadikonda@users.noreply.github.com>
2025-08-22 17:25:14 +0530

    Update Test33B.java

--- Test33B.java
+++ Test33B.java
@@ -1,21 +1,21 @@
-/* program to print 1 to n numbers */
-package corejavaprograms;
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
 
-public class Test31
+class Main {
+    public static void main(String[] args)
+    {
+int i=65;
+while(i<=69)
 {
-	public static void main(String args[])
-{
-int ch1=65;
-while(ch1<=69)
-{
-	int ch2=65;
-	while(ch2<=69)
-	{	
-System.out.print("\t"+(char)(ch1));
-ch2=ch2+1;
+    int j=65;
+    while(j<=69)
+    {
+        System.out.print("\t"+(char)(i));
+        j=j+1;
+    }
+    i=i+1;
+    System.out.println();
+
 }
-	ch1=ch1+1;
-	System.out.println();
-}
-}
+    }
 }
