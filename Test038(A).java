57586a09f7c0fb03941e278da70c902f9f343abc
SRINADH TADIKONDA <44929625+srinadhtadikonda@users.noreply.github.com>
2025-09-01 17:31:29 +0530

    Create Test038(A).java

---
+++ Test038(A).java
@@ -0,0 +1,16 @@
+
+public class Test38
+{
+	public static void main(String args[])
+	{
+		int a[]=new int[5];
+		a[0]=10;
+		a[1]=20;
+		a[2]=30;
+		a[3]=40;
+		a[4]=50;
+		System.out.print(a[0]+"\t"+a[1]+"\t"+a[2]+"\t"+a[3]+"\t"+a[4]);
+	}
+
+}
+
