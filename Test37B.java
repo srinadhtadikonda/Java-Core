1cbd978cba70a838ccb4639c00cc64453ab75573
SRINADH TADIKONDA <44929625+srinadhtadikonda@users.noreply.github.com>
2025-11-25 19:35:34 +0530

    Create Test37B.java

---
+++ Test37B.java
@@ -0,0 +1,18 @@
+package corejavaprograms;
+
+import java.lang.*;
+import java.util.*;
+import java.math.*;
+public class Test 
+{
+public static void main(String args[])
+	{
+		int a[]=new int[5];
+		a[0]=10;
+		a[1]=20;
+		a[2]=30;
+		a[3]=40;
+		a[4]=50;
+	System.out.print("\t"+a[0]+"\t"+a[1]+"\t"+a[2]+"\t"+a[3]+"\t"+a[4]);
+}
+}
