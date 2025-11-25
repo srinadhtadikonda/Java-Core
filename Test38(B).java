06cba4b53dcbdff09dccd2f6b4b013dd16a75c19
SRINADH TADIKONDA <44929625+srinadhtadikonda@users.noreply.github.com>
2025-11-25 19:45:23 +0530

    Create Test38(B).java

---
+++ Test38(B).java
@@ -0,0 +1,22 @@
+package corejavaprograms;
+
+import java.lang.*;
+import java.util.*;
+import java.math.*;
+public class Test 
+{
+public static void main(String args[])
+	{
+		int a[]=new int[5];
+		a[0]=10;
+		a[1]=20;
+		a[2]=30;
+		a[3]=40;
+		a[4]=50;
+		int i;
+		for(i=0;i<a.length;i++)
+		{
+			System.out.print("\t"+a[i]);
+		}
+}
+}
