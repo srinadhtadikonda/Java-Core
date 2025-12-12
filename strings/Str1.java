3d534afa237389c6b4f1011306aebb3b51e99412
SRINADH TADIKONDA <44929625+srinadhtadikonda@users.noreply.github.com>
2025-12-12 12:33:05 +0530

    Create Str1.java

---
+++ strings/Str1.java
@@ -0,0 +1,13 @@
+package corejavaprograms;
+
+public class Test {
+    public static void main(String[] args) {
+
+        String[] arr = { "State Bank of India" };
+
+        // Loop, but runs only once because array has one element
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("String = " + arr[i]);
+        }
+    }
+}
