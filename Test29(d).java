792359a600094e563c91c6449e6025d83328f8b0
SRINADH TADIKONDA <44929625+srinadhtadikonda@users.noreply.github.com>
2026-06-06 17:17:55 +0530

    Create Test29(d).java

---
+++ Test29(d).java
@@ -0,0 +1,27 @@
+import java.lang.*;
+import java.io.*;
+class Main 
+{
+    public static void main(String args[]) throws IOException 
+    {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        int i=2,n;
+        System.out.print("Enter a number..:");
+        n=Integer.parseInt(br.readLine());
+        if(n<=1)
+        {
+            System.out.println("Not a Prime");
+            return;
+        }
+        while(i<=n/2)
+        {
+            if(n%i==0)
+        {
+            System.out.println("Not a prime");
+            return;
+        }
+        i++;
+        }
+        System.out.println("prime");
+    }
+}
