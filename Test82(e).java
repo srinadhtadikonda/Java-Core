5fb529618441af980c2e002294ae04eac8f48cdc
SRINADH TADIKONDA <44929625+srinadhtadikonda@users.noreply.github.com>
2026-06-19 07:56:14 +0530

    Create Test82(e).java

---
+++ Test82(e).java
@@ -0,0 +1,39 @@
+package corejavaprograms;
+
+class InvalidAgeException extends Exception
+{
+    public String toString()
+    {
+        return "You Are Not Eligible To Vote";
+    }
+}
+
+class Test
+{
+    static void ageCheck(int age) throws InvalidAgeException
+    {
+        if(age < 18)
+        {
+            throw new InvalidAgeException();
+        }
+        else
+        {
+            System.out.println("You Are Eligible To Vote");
+        }
+    }
+}
+
+class CheckAge
+{
+    public static void main(String args[])
+    {
+        try
+        {
+            Test.ageCheck(5);   // Calling static method
+        }
+        catch(InvalidAgeException ie)
+        {
+            System.out.println(ie);
+        }
+    }
+}
