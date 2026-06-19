ad61a56a3eaca344aa828d9f4bec784cd31e2b53
SRINADH TADIKONDA <44929625+srinadhtadikonda@users.noreply.github.com>
2026-06-19 08:16:34 +0530

    Update Test82(c).java

--- Test82(c).java
+++ Test82(c).java
@@ -1,40 +1,29 @@
 package corejavaprograms;
 
-class InvalidAgeException extends Exception
-{
-	public String toString()
-	{
-		return "You Are Not Eligible To Vote";
-		
-	}
+class InvalidAgeException extends Exception {
+    InvalidAgeException() {
+        super("You Are Not Eligible To Vote");
+    }
 }
-class Test 
-{
-void ageCheck(int age) throws InvalidAgeException
-{
-	if(age<18)
-{
-		throw new InvalidAgeException();
-}
-else
-{
-	System.out.println("You Are  Eligible To Vote");
-}
-}
-}
-class CheckAge
-{
-public static void main(String args[])
-{
-Test T=new Test();
-	try
-	{
-		T.ageCheck(5);
-	}catch(InvalidAgeException ie)
-	{
-		System.out.println(ie);
-	}
-	}
-}
----------------------------------------------------------------------------------------
 
+class Test {
+    void ageCheck(int age) throws InvalidAgeException {
+        if (age < 18) {
+            throw new InvalidAgeException();
+        } else {
+            System.out.println("You Are Eligible To Vote");
+        }
+    }
+}
+
+public class CheckAge {
+    public static void main(String[] args) {
+        Test t = new Test();
+
+        try {
+            t.ageCheck(5);
+        } catch (InvalidAgeException ie) {
+            System.out.println(ie.getMessage());
+        }
+    }
+}
