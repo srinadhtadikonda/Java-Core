b8e65d7abb322b5d39b2fa4c4d0deb0843915c20
SRINADH TADIKONDA <44929625+srinadhtadikonda@users.noreply.github.com>
2026-06-26 08:28:59 +0530

    Create St01.java

---
+++ Strings/St01.java
@@ -0,0 +1,9 @@
+public class PrintStringArray {
+    public static void main(String[] args) {
+        char[] arr = {'H', 'e', 'l', 'l', 'o'};
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+        }
+    }
+}
