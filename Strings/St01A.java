83476aafa4450a7d6145a5e4947c0cfac815d8af
SRINADH TADIKONDA <44929625+srinadhtadikonda@users.noreply.github.com>
2026-06-26 08:41:24 +0530

    Refactor PrintStringArray to Main class
    Refactored PrintStringArray class to Main and updated input handling.

--- Strings/St01A.java
+++ Strings/St01A.java
@@ -1,14 +1,26 @@
-import java.util.Scanner;
+import java.util.*;
 
-public class PrintStringArray {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+class Main {
+    public static void main(String args[]) {
+        Scanner SC = new Scanner(System.in);
+        String x;
 
-        System.out.print("Enter characters: ");
-        char[] arr = sc.next().toCharArray();
+        System.out.print("Enter String..: ");
+        x = SC.nextLine();
 
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i]);
+        int len = 0;
+
+        // finding length using loop
+        try {
+            for (;;) {
+                x.charAt(len);
+                len++;
+            }
+        } catch (Exception e) {
+            // loop stops when index goes out of range
         }
+
+        System.out.println("String = " + x);
+        System.out.println("Length = " + len);
     }
 }
