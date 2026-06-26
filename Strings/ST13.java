88113566ae05b50981a3e20af72adc8f1305c0ad
SRINADH TADIKONDA <44929625+srinadhtadikonda@users.noreply.github.com>
2026-06-26 08:35:06 +0530

    Create ST13.java

---
+++ Strings/ST13.java
@@ -0,0 +1,25 @@
+public class MostRepeatedChar {
+    public static void main(String[] args) {
+        String str = "banana";
+
+        int max = 0;
+        char result = ' ';
+
+        for (int i = 0; i < str.length(); i++) {
+            int count = 0;
+
+            for (int j = 0; j < str.length(); j++) {
+                if (str.charAt(i) == str.charAt(j)) {
+                    count++;
+                }
+            }
+
+            if (count > max) {
+                max = count;
+                result = str.charAt(i);
+            }
+        }
+
+        System.out.println("Most repeated: " + result);
+    }
+}
