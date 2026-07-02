eacd5968ec07c17f6b30617891be9064415fbbd9
SRINADH TADIKONDA <44929625+srinadhtadikonda@users.noreply.github.com>
2026-07-02 11:29:58 +0530

    Create Swing01.java

---
+++ Swings/ClassWork/Swing01.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+class Swing01
+{
+    public static void main(String args[])
+    {
+        JFrame J=new JFrame("My App");
+        J.setSize(400,500);
+        J.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        J.setVisible(true);
+    }
+}
